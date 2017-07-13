eb46ad93c587d47574f9529f4df89213bc02520f
ahorod <prozerpina112009@gmail.com>
2017-07-13 00:11:09 -0700

    like/unlike with ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,22 +1,4 @@
 class FavoritesController < ApplicationController
-  def new
-    @account = current_user.account
-    @favorite = @account.favorites.new(product_id: params[:product_id])
-    @favorite.save
-    respond_to do |format|
-      format.html {redirect_to products_path}
-      format.js
-    end
-  end
 
-  def destroy
-    @favorite = Favorite.find(params[:id])
-    @favorite.destroy
-    respond_to do |format|
-      format.html {redirect_to products_path}
-      format.js
-    end
-
-  end
 
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
 class ProductsController < ApplicationController
-  before_action :except => [:index, :show] do
+  before_action :except => [:index, :show, :like, :unlike] do
     redirect_to new_user_session_path unless current_user && current_user.admin
   end
+  before_action :only => [:like, :unlike] do
+    redirect_to new_user_session_path unless current_user
+  end
+  helper_method :is_product_liked
 
   def index
     @products = Product.all
@@ -21,6 +25,33 @@ class ProductsController < ApplicationController
   def create
     @product = Product.create!(product_params)
     redirect_to products_path
+  end
+
+  def like
+    @product_id = params[:id]
+    @account = current_user.account
+    @favorite = @account.favorites.new(product_id: params[:id])
+    @favorite.save
+    respond_to do |format|
+      format.html {redirect_to products_path}
+      format.js
+    end
+  end
+
+  def unlike
+    @product_id = params[:id]
+    @account = current_user.account
+    @favorite = @account.favorites.where("product_id = ?", params[:id])
+    @favorite.each { |x| x.destroy! }
+    # magic line
+    respond_to do |format|
+      format.html {redirect_to products_path}
+      format.js
+    end
+  end
+
+  def is_product_liked(product)
+    current_user.account.favorites.where("product_id = ?", product.id).count > 0
   end
 
 private
